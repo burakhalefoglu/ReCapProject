0fbbc5b9c524df60956b41fd20a496ee2228e346
burakhalefoglu <42536490+burakhalefoglu@users.noreply.github.com>
2021-03-11 15:20:52 +0300

    Lesson 10 - Ödev 4
    Lesson 10 - Ödev 4

---
+++ Business/Abstract/IRentalsService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IRentalsService
+    {
+        IDataResult<List<Rentals>> GetAllRentals();
+        IResult UpdateRental(Rentals rental);
+        IResult DeleteRental(Rentals car);
+        IResult AddRental(Rentals rental);
+        IDataResult<Rentals> GetById(int id);
+    }
+}
---
+++ Business/Concrete/RentalsManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalsManager : IRentalsService
+    {
+        IRentalsDal rentalDal;
+
+        public RentalsManager(IRentalsDal rentalDal)
+        {
+            this.rentalDal = rentalDal;
+        }
+
+        public IResult AddRental(Rentals rental)
+        {
+            rentalDal.Add(rental);
+            return new SuccessResult();
+        }
+
+        public IResult DeleteRental(Rentals rental)
+        {
+            rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rentals>> GetAllRentals()
+        {
+            var result = rentalDal.GetAll();
+            return new SuccessDataResult<List<Rentals>>(result);
+        }
+
+        public IDataResult<Rentals> GetById(int id)
+        {
+            var result = rentalDal.Get(r=> r.CustomerId ==id);
+            return new SuccessDataResult<Rentals>(result);
+        }
+
+        public IResult UpdateRental(Rentals rental)
+        {
+            rentalDal.Update(rental);
+            return new SuccessResult();
+        }
+    }
+}
